a4178e590ca3962d62139564d3e3a6aad693e381
goto-gs <0103564www@gmail.com>
2020-02-03 15:18:50 +0900

    stamp_image_add

--- server.js
+++ server.js
@@ -29,23 +29,46 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 
+// botが返信する内容を決めとる関数
 function handleEvent(event) {
+    //送られてきたメッセージがテキスト、画像以外なら何もしない
     if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
+        if (event.message.type !== 'image') {
+            return Promise.resolve(null);
+        }
     }
 
-    let replyText = '';
-    if (event.message.text === '武田') {
-        replyText = '紀之';
+    // event.message.type(受信したメッセ)が画像だったときは、カワイイと言ってハートのスタンプを送る
+    if (event.message.type == 'image') {
+        // event.replyToken（リターンメッセージ）が配列[]になっているのがポイント
+        return client.replyMessage(event.replyToken, [{
+            type: 'text',
+            text: 'カワイイ'
+        },
+        {
+            type: 'sticker',
+            packageId: '11538',
+            stickerId: '51626495' //スタンプはIdで変更可能
+        }]);
     } else {
-        replyText = '知らない人ですね';
+        let replyText = '';
+        if (event.message.text === '好き') {
+            replyText = '俺も';
+        } else if (event.message.text === '大好き') {
+            replyText = '俺の方が好きだ';
+        } else {
+            replyText = '君の顔が見たいな。写真送ってよ。';
+        }
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: replyText //返信する内容
+        });
     }
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: replyText //実際に返信の言葉を入れる箇所
-    });
 }
 
+// これはローカルの確認用で使ったやつ
 // app.listen(PORT);
+
+// ngrokはこれ
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
 console.log(`Server running at ${PORT}`);
